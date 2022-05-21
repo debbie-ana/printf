8ab71269628eff10bbed84fb048f0134defaaff1
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-20 17:18:26 -0700

    buffer

--- _putchar.c
+++ _putchar.c
@@ -10,33 +10,5 @@
 
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _puts - prints a string
- * @str: pointer to string
- * Return: numbeer of char
- */
-
-int _puts(char *str)
-{
-	register int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+	return (write(1, &c, 1));
 }
