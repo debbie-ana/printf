ec7660155faa5b59273da3655758085ecb47c111
Deborah <semiloremuriana@gmail.com>
2022-05-22 18:04:15 -0700

    task 4

--- _putchar.c
+++ _putchar.c
@@ -5,10 +5,25 @@
  * _putchar - to print a character
  * @c: character to be printed
  *
+ * Description - updated to use a local buffer of 1024 chars
+ * in order to call write as little as possible.
  * Return: written character
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buff[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buff, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buff[i] = c;
+		i++;
+	}
+	return (1);
 }
