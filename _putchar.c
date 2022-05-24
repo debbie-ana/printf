5515681fb26c96eed3fb854f3832f67b0cda58f8
Deborah <semiloremuriana@gmail.com>
2022-05-23 17:45:12 -0700

    b2b again

--- _putchar.c
+++ _putchar.c
@@ -12,7 +12,7 @@
 
 int _putchar(char c)
 {
-	static char buff[1024];
+	/* static char buff[1024];
 	static int i;
 
 	if (c == -1 || i >= 1024)
@@ -24,6 +24,6 @@ int _putchar(char c)
 	{
 		buff[i] = c;
 		i++;
-	}
-	return (1);
+	}*/
+	return (write(1, &c, 1));
 }
