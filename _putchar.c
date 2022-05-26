b9f99746147bb024e4a9595f26f50540f49f76e1
Deborah <semiloremuriana@gmail.com>
2022-05-25 17:13:44 -0700

    task five

--- _putchar.c
+++ _putchar.c
@@ -12,20 +12,7 @@
 
 int _putchar(char c)
 {
-	static char buff[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buff, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buff[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
 
 /**
